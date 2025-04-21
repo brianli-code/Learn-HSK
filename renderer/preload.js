8512e704a26be51b49ac33a845678a07b39ce49b
Brian <brianli4824@gmail.com>
2025-04-20 17:48:52 -0700

    intro

---
+++ renderer/preload.js
@@ -0,0 +1,11 @@
+// const fs = require('fs');
+// const path = require('path');
+const { contextBridge } = require('electron');
+
+contextBridge.exposeInMainWorld('hskDataAPI', {
+  getHSKData: () => {
+    const dataPath = path.join(__dirname, 'data', 'hsk3.json');
+    const jsonData = fs.readFileSync(dataPath);
+    return JSON.parse(jsonData);
+  }
+});
